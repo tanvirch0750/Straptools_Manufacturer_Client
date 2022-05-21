2f90fd659dc65bed3f405a98aa5e1114a15021cc
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-22 03:51:38 +0600

    implemented feature product section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner";
+import FeatureProduct from "../components/FeatureProduct";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <FeatureProduct />
     </>
   );
 };
---
+++ src/components/FeatureProduct.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import featurImgPlastic from "../assets/images/feature/plastic-feature.png";
+import featurImgSteel from "../assets/images/feature/steel-feature.png";
+import featurImgTextile from "../assets/images/feature/textile.png";
+import "../styles/FeatureProduct.css";
+
+const FeatureProduct = () => {
+  return (
+    <section className="feature-products">
+      <div className="feature-products-inner container">
+        <div className="section-heading">
+          <h2>Our Product Range</h2>
+        </div>
+
+        <div className="feature-products-container">
+          <div className="feature-product">
+            <h3>STRAPPING TOOL FOR PLASTIC STRAP</h3>
+            <img src={featurImgPlastic} alt="plastic starp" />
+            <button className="btn btn-full">View All</button>
+          </div>
+          <div className="feature-product">
+            <h3>STRAPPING TOOL FOR STEEL STRAP</h3>
+            <img src={featurImgSteel} alt="steel starp" />
+            <button className="btn btn-full">View All</button>
+          </div>
+          <div className="feature-product">
+            <h3>STRAPPING TOOL FOR TEXTILE STRAP</h3>
+            <img src={featurImgTextile} alt="texttile starp" />
+            <button className="btn btn-full">View All</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeatureProduct;
