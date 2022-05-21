098c5a1db5facc8caaf7f52a20e7af4d45943a40
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-22 01:49:45 +0600

    implemented header navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
 import HeaderContact from "./HeaderContact";
 
 const Header = () => {
   return (
     <header>
       <HeaderContact />
+      <nav className="nav">
+        <div className="container nav-inner">
+          <div className="nav-logo">
+            <h2>
+              <Link to="/">STRAPTOOLS</Link>
+            </h2>
+          </div>
+          <div className="nav-navigation">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+              <li>
+                <Link to="/contactUs">Contact Us</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-button">
+            <button className="btn btn-full">Login</button>
+            <button className="btn btn-outline">Signup</button>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 };
