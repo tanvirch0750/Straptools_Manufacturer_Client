b9714549c82365fae1599b4528c628ad39c33cac
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-22 23:37:20 +0600

    implemented loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="yellow"></div>
+      <div className="red"></div>
+      <div className="blue"></div>
+      <div className="violet"></div>
+    </div>
+  );
+};
+
+export default Loading;
