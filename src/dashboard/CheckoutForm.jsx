cace8abfab1963205769fe1dd5238be7c130dc4f
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-25 02:46:25 +0600

    implemented payment functionality

---
+++ src/dashboard/CheckoutForm.jsx
@@ -0,0 +1,143 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from "react";
+import "../styles/CheckoutForm.css";
+
+const CheckoutForm = ({ order }) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
+  const [processing, setProcessing] = useState(false);
+
+  const { _id, totalPrice, email, userName } = order;
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:5000/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify({ totalPrice }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.clientSecret) {
+          setClientSecret(data.clientSecret);
+        }
+      });
+  }, [totalPrice]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const card = elements.getElement(CardElement);
+
+    if (card == null) {
+      return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+
+    setCardError(error?.message || "");
+    setSuccess("");
+    // setProcessing(true);
+
+    // Confirmed card payment
+    const { paymentIntent, error: intentError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            name: userName,
+            email: email,
+          },
+        },
+      });
+
+    if (intentError) {
+      setCardError(intentError?.message);
+      // setProcessing(false);
+    } else {
+      setCardError("");
+      // console.log(paymentIntent);
+      setTransactionId(paymentIntent.id);
+      setSuccess("Congrats your payment is completed");
+
+      // update data and set data into db
+      const payment = {
+        order: _id,
+        transactionId: paymentIntent.id,
+      };
+
+      const url = `http://localhost:5000/order/${_id}`;
+
+      fetch(url, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body: JSON.stringify(payment),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          // setProcessing(false);
+        });
+    }
+  };
+
+  // if (setProcessing) {
+  //   return <Loader />;
+  // }
+  return (
+    <form onSubmit={handleSubmit}>
+      <CardElement
+        options={{
+          style: {
+            base: {
+              fontSize: "16px",
+              color: "#705c42",
+              "::placeholder": {
+                color: "#aab7c4",
+              },
+            },
+            invalid: {
+              color: "#9e2146",
+            },
+          },
+        }}
+      />
+      <button
+        className="btn btn-full checkout-btn"
+        type="submit"
+        disabled={!stripe || !clientSecret}
+      >
+        Pay
+      </button>
+
+      {cardError && <p className="error-message">{cardError}</p>}
+      {success && (
+        <div className="payment-text-success">
+          <p>{success}</p>
+          <p>
+            Your Transaction Id:{" "}
+            <span className="payment-trx">{transactionId}</span>
+          </p>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default CheckoutForm;
--- src/dashboard/Payment.jsx
+++ src/dashboard/Payment.jsx
@@ -1,11 +1,50 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import Loading from "../components/Loading";
+import "../styles/Payment.css";
+import CheckoutForm from "./CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51JqdAsGXrwkMLuhtiJgMQKnaLLHsfi7RQMEznBz9DWD4EQxzjKqGqjajqHa3EMBXLxTM0Bf6WiGAAigES9a0YSB600FrqYDOoX"
+);
 
 const Payment = () => {
   const { id } = useParams();
+
+  const { data: order, isLoading } = useQuery(["paymentOrder", id], () =>
+    fetch(`http://localhost:5000/orders/${id}`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    }).then((res) => res.json())
+  );
+
+  console.log(order);
+
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <section className="payment">
-      <h1>Payment {id}</h1>
+      <div className="section-heading">
+        <h2>Make Your Payment</h2>
+      </div>
+      <div className="payment-order-details">
+        <h4>Hello {order.userName}</h4>
+        <h3>{order.name}</h3>
+        <p>Quantity: {order.quantity}</p>
+        <p>Price: ${order.price}</p>
+        <p>Pleae Pay: ${order.totalPrice}</p>
+      </div>
+      <div className="payment-order-stripe">
+        <Elements stripe={stripePromise}>
+          <CheckoutForm order={order} />
+        </Elements>
+      </div>
     </section>
   );
 };
