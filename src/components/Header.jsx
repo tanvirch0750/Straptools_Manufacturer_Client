d1509b7f4dcda4b4a398b677820cdb1b3892dd00
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-26 02:26:22 +0600

    make header nav responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { IoCloseOutline, IoMenuOutline } from "react-icons/io5";
 import { Link, useLocation } from "react-router-dom";
 import auth from "../Firebase.init";
 import "../styles/Header.css";
@@ -8,35 +9,36 @@ import NavProfile from "./NavProfile";
 const Header = () => {
   const [user] = useAuthState(auth);
   const location = useLocation();
+  const [open, setOpen] = useState(false);
 
   return (
     <header>
-      <nav className="nav">
+      <nav className={`nav ${open ? "open" : ""}`}>
         <div className="container nav-inner">
           <div className="nav-logo">
             <h2>
               <Link to="/">STRAPTOOLS</Link>
             </h2>
           </div>
-          <div className="nav-navigation">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/products">Products</Link>
-              </li>
-              <li>
-                <Link to="/blogs">Blogs</Link>
-              </li>
-              <li>
-                <Link to="/portfolio">My Portfolio</Link>
-              </li>
-            </ul>
-          </div>
+
+          <ul className="nav-navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">My Portfolio</Link>
+            </li>
+          </ul>
+
           <div className="nav-button">
             {!user && (
               <>
@@ -48,21 +50,12 @@ const Header = () => {
                 </Link>
               </>
             )}
-
+          </div>
+          <div className="header-profile-box">
             {user && <NavProfile />}
-            {/* {user ? (
-              <Link
-                to="/login"
-                className="btn btn-full"
-                onClick={handleSignOut}
-              >
-                Signout
-              </Link>
-            ) : (
-              <Link to="/signup" className="btn btn-outline">
-                Signup
-              </Link>
-            )} */}
+            <button className="hamburger-btn" onClick={() => setOpen(!open)}>
+              {open ? <IoCloseOutline /> : <IoMenuOutline />}
+            </button>
           </div>
         </div>
       </nav>
