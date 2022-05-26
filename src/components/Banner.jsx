3dd1e3cb08fca4aa98b707b15c423ed81e05c2e8
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-26 19:30:03 +0600

    add link to the products page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "../styles/Banner.css";
 
 const Banner = () => {
+  const navigate = useNavigate();
   return (
     <section className="banner">
       <div className="banner-inner container">
@@ -9,7 +11,12 @@ const Banner = () => {
           <h1>We Brand Quality. Quality leads with the industry.</h1>
           <p>Strapping tools for plastic, steel and textile starps.</p>
           <div className="banner-btn">
-            <button className="btn btn-full">All Products</button>
+            <button
+              className="btn btn-full"
+              onClick={() => navigate("/products")}
+            >
+              All Products
+            </button>
           </div>
         </div>
       </div>
--- src/components/FeatureProduct.jsx
+++ src/components/FeatureProduct.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import featurImgPlastic from "../assets/images/feature/plastic-feature.png";
 import featurImgSteel from "../assets/images/feature/steel-feature.png";
 import featurImgTextile from "../assets/images/feature/textile.png";
 import "../styles/FeatureProduct.css";
 
 const FeatureProduct = () => {
+  const navigate = useNavigate();
   return (
     <section className="feature-products">
       <div className="feature-products-inner container">
@@ -16,17 +18,32 @@ const FeatureProduct = () => {
           <div className="feature-product">
             <h3>STRAPPING TOOL FOR PLASTIC STRAP</h3>
             <img src={featurImgPlastic} alt="plastic starp" />
-            <button className="btn btn-full">View All</button>
+            <button
+              className="btn btn-full"
+              onClick={() => navigate("/products")}
+            >
+              View All
+            </button>
           </div>
           <div className="feature-product">
             <h3>STRAPPING TOOL FOR STEEL STRAP</h3>
             <img src={featurImgSteel} alt="steel starp" />
-            <button className="btn btn-full">View All</button>
+            <button
+              className="btn btn-full"
+              onClick={() => navigate("/products")}
+            >
+              View All
+            </button>
           </div>
           <div className="feature-product">
             <h3>STRAPPING TOOL FOR TEXTILE STRAP</h3>
             <img src={featurImgTextile} alt="texttile starp" />
-            <button className="btn btn-full">View All</button>
+            <button
+              className="btn btn-full"
+              onClick={() => navigate("/products")}
+            >
+              View All
+            </button>
           </div>
         </div>
       </div>
